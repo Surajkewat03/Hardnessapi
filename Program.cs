b0f46ff83947658d301e2abc670d7e844338ef43
suraj kewat <jemittal0@gmail.com>
2026-03-09 13:52:34 +0530

    fix render startup crash

--- Program.cs
+++ Program.cs
@@ -3,27 +3,18 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Controllers
 builder.Services.AddControllers();
 
-// Database Connection
 builder.Services.AddDbContext<HardnessDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("HardnessDb")));
 
-// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Swagger
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
 
